1a8f5c6f404207008311fec03b350b51c4cdbd98
Jameswiggins987 <124939185+Jameswiggins987@users.noreply.github.com>
2023-05-26 12:09:56 +1200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,34 @@
+let item = 0
+input.onGesture(Gesture.SixG, function () {
+    item = randint(0, 2)
+    if (item == 0) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    }
+    if (item == 2) {
+        basic.showLeds(`
+            # # . . #
+            # # . # .
+            . . # . .
+            # # . # .
+            # # . . #
+            `)
+    }
+    if (item == 2) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    }
+})
+basic.forever(function () {
+	
+})
